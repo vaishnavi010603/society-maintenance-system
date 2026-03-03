70ab908e76106624f387c16c9612ce53c2ab725b
vaishnavi010603 <vaishnavi010603@gmail.com>
2026-03-03 20:27:55 +0530

    Refactor backup script to improve file handling

--- web/backup.php
+++ web/backup.php
@@ -12,27 +12,37 @@ $zip = new ZipArchive();
 
 if ($zip->open($zipname, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) {
 
-    $files = new RecursiveIteratorIterator(
-        new RecursiveDirectoryIterator("data"),
-        RecursiveIteratorIterator::LEAVES_ONLY
-    );
+    // Make sure data folder exists
+    if (is_dir("data")) {
 
-    foreach ($files as $name => $file) {
-        if (!$file->isDir()) {
-            $filePath = $file->getRealPath();
-            $relativePath = substr($filePath, strlen(realpath("data")) + 1);
-            $zip->addFile($filePath, $relativePath);
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator("data"),
+            RecursiveIteratorIterator::LEAVES_ONLY
+        );
+
+        foreach ($files as $file) {
+
+            if (!$file->isDir()) {
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen(realpath("data")) + 1);
+                $zip->addFile($filePath, $relativePath);
+            }
         }
     }
 
     $zip->close();
 
+    // Download ZIP
     header('Content-Type: application/zip');
-    header('Content-Disposition: attachment; filename="'.$zipname.'"');
+    header('Content-Disposition: attachment; filename="' . basename($zipname) . '"');
     header('Content-Length: ' . filesize($zipname));
-
     readfile($zipname);
+
+    // Delete zip after download
     unlink($zipname);
-    exit;
+
+    exit();
+
+} else {
+    echo "Backup failed!";
 }
-?>
