89afd26876adca2ff9eeb4b31770fc14dbb86dc3
vaishnavi010603 <vaishnavi010603@gmail.com>
2026-03-03 21:05:09 +0530

    Simplify logout process by removing unnecessary code
    Removed session variable unset and comments.

--- web/logout.php
+++ web/logout.php
@@ -1,13 +1,6 @@
 <?php
 session_start();
-
-// Unset all session variables
-$_SESSION = [];
-
-// Destroy the session
 session_destroy();
-
-// Redirect to login page
 header("Location: login.php");
 exit();
 ?>
